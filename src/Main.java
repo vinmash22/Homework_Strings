43f788edcf904c7b3e3f68bcc8c82275d4c53f88
vinmash <vinmash@mail.ru>
2023-02-04 01:08:29 +0500

    Strings

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,34 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        System.out.println("");
+        task2();
+        System.out.println("");
+        task3();
+    }
+
+    public static void task1() {
+        System.out.println("Задача 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = "ФИО сотрудника - " + lastName + " " + firstName + " " + middleName;
+        System.out.println(fullName);
+
+    }
+
+    public static void task2() {
+        System.out.println("Задача 2");
+        String fullName = "Ivanov Ivan Ivanovich";
+        System.out.println("Данные ФИО сотрудника для заполнения отчета - " + fullName.toUpperCase());
+
+    }
+
+    public static void task3() {
+        System.out.println("Задача 3");
+        String fullName = "Иванов Семён Семёнович";
+        String rightFullName = fullName.replace("ё", "е");
+        System.out.println("Данные ФИО сотрудника - " + rightFullName);
+
     }
 }
